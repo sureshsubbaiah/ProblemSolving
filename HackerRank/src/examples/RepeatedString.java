1b5a13031af755d1ab19aa7aa7a2a980137c194c
Suresh Subbaiah <sureshs5@in.ibm.com>
2019-07-31 19:46:23 +0530

    repeated string

---
+++ HackerRank/src/examples/RepeatedString.java
@@ -0,0 +1,60 @@
+package examples;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ */
+public class RepeatedString {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+
+        char[] a=s.toCharArray();
+        long count=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]=='a'){
+                count++;
+            }
+        }
+
+        long repetitions = n/a.length;
+        count = repetitions * count;
+
+
+        for(int i =0; i< n%a.length ; i++){
+            if(a[i]=='a'){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
